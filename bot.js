693795203b1ff5e8fdee88927f22526ec4b2e356
cyla00 <ikhayam000@protonmail.com>
2020-09-15 14:15:00 +0200

    start*

--- bot.js
+++ bot.js
@@ -5,7 +5,7 @@ const Bot = new Discord.Client();
 const Token = process.env.TOKEN;
 
 Bot.on('ready', () => {
-  console.log(`${Bot.user.username} online...\nPresence in server ${Bot.guilds.cache.size}`);
+  console.log(`${Bot.user.username} online...\nPresence in servers ${Bot.guilds.cache.size}`);
 });
 
 Bot.login(Token);
